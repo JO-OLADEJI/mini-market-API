da6b2c94b3f11b0a4302264dc7e7ad9298d4bb45
Joshua Oladeji <dejijolaoluwa@gmail.com>
2021-06-22 18:06:58 +0100

    created and tested logic for retrieving market(s)

--- controllers/marketController.js
+++ controllers/marketController.js
@@ -1,27 +1,46 @@
 const mongoose = require('mongoose');
-
+const Market = require('../models/marketModel.js');
 
 
 
 class MarketController {
 
-  createMarket = async () => {
+  createMarket = async (req, res) => {
     // handle the creation of a new market
   }
 
-  readMarkets = async () => {
+
+  readMarkets = async (req, res) => {
     // returns an array of all the markets
+    try {
+      const allMarkets = await Market.find();
+      res.json(allMarkets);
+    }
+    catch(err) {
+      res.send(err);
+    }
   }
 
-  readMarket = async () => {
+
+  readMarket = async (req, res) => {
     // returns a specific market by ID
+    try {
+      const { id } = req.params;
+      const reqMarket = await Market.findById(id);
+      res.json(reqMarket);
+    }
+    catch (err) {
+      res.send(err);
+    }
   }
 
-  updateMarket = async () => {
+
+  updateMarket = async (req, res) => {
     // handles the updating of an existing market
   }
 
-  deleteMarket = async () => {
+
+  deleteMarket = async (req, res) => {
     // handles the deletion of a specific market by ID
   }
 
